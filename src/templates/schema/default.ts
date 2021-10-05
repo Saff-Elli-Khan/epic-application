111572a3406c19aa124b07ad4748097c8b5fe770
Saif Ali Khan <saffellikhan@gmail.com>
2021-10-05 22:41:17 +0500

    Create default.ts

---
+++ src/templates/schema/default.ts
@@ -0,0 +1,27 @@
+/* <ImportsTemplate> import { {{ modules }} } from "{{ location }}"; </ImportsTemplate> */
+/* <ColumnTemplate> @Column({{ options }}) {{ name }}!: {{ datatype }}; </ColumnTemplate> */
+/* <OneRelationTemplate> @OneRelation<{{ relation }}, {{ schema }}>({ schema: () => {{ relation }}, mapping: {{ mapping }} }) {{ name }}!: {{ relation }}; </OneRelationTemplate> */
+/* <ManyRelationTemplate> @ManyRelation<{{ relation }}, {{ schema }}>({ schema: () => {{ relation }}, mapping: {{ mapping }} }) {{ name }}!: {{ relation }}[]; </ManyRelationTemplate> */
+
+import {
+  Schema,
+  BaseSchema,
+  Column,
+  OneRelation,
+  ManyRelation,
+  Utils,
+} from "@saffellikhan/epic-sql";
+/* @ImportsContainer */
+/* /ImportsContainer */
+
+@Schema()
+export class Sample extends BaseSchema {
+  /* @ColumnsContainer */
+
+  /* <SampleIdColumn[ColumnTemplate]> */
+  @Column({ index: ["UNIQUE"], defaultValue: () => Utils.uuidShort() })
+  SampleId!: number;
+  /* </SampleIdColumn> */
+
+  /* /ColumnsContainer */
+}
