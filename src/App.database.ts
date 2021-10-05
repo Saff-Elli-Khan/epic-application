fcb34f86e0eca00d78db495f172aefceff6f7823
Saif Ali Khan <saffellikhan@gmail.com>
2021-10-05 22:03:42 +0500

    Miner updates

---
+++ src/App.database.ts
@@ -0,0 +1,33 @@
+/* <ImportsTemplate> import { {{ modules }} } from "{{ location }}"; </ImportsTemplate> */
+/* <SchemaListTemplate> {{ schema }}, </SchemaListTemplate> */
+
+import { Connection, EpicSQLManager } from "@saffellikhan/epic-sql";
+import { ConfigManager } from "@saffellikhan/epic-cli";
+/* @ImportsContainer */
+/* /ImportsContainer */
+
+// Create A New Database Connection.
+export const createDatabaseConnection = (sync = false, logs = false) =>
+  new EpicSQLManager(
+    new Connection(
+      (() => {
+        // Get Connection Details
+        const ConnectionDetails = ConfigManager.getConfig("main").database;
+
+        return {
+          host: ConnectionDetails.host,
+          port: ConnectionDetails.port,
+          user: ConnectionDetails.user,
+          password: ConnectionDetails.password,
+          database: ConnectionDetails.dbname,
+        };
+      })()
+    ),
+    [
+      /* @SchemaListContainer */
+      /* /SchemaListContainer */
+    ]
+  ).init({
+    sync,
+    logs,
+  });
