09efbe67647b6e2f49c8a1d8c00c6c4bcb2647c4
Saif Ali Khan <saffellikhan@gmail.com>
2022-05-20 18:00:27 +0500

    Update globals.ts

--- src/core/globals.ts
+++ src/core/globals.ts
@@ -227,12 +227,14 @@ export const DatabaseDriver = new MongoDBDriver(
 );
 
 // Create Redis Client
-export const RedisClient = new Redis({
-  host: process.env.REDIS_HOST,
-  port: parseInt(process.env.REDIS_PORT || "6379"),
-  username: process.env.REDIS_USERNAME,
-  password: process.env.REDIS_PASSWORD,
-});
+export const RedisClient = process.env.REDIS_HOST
+  ? new Redis({
+      host: process.env.REDIS_HOST,
+      port: parseInt(process.env.REDIS_PORT || "6379"),
+      username: process.env.REDIS_USERNAME,
+      password: process.env.REDIS_PASSWORD,
+    })
+  : undefined;
 
 // Create a Cron Scheduler Instance
 export const Schedule = new Schedular({
