a808e6516892c68129c6ee80ba0f81b7fb750f7a
Saif Ali Khan <saffellikhan@gmail.com>
2022-06-16 15:59:01 +0500

    Update syncPostman.ts

--- src/core/syncPostman.ts
+++ src/core/syncPostman.ts
@@ -1,10 +1,4 @@
-import Path from "path";
 import { HTTPServer } from "./server";
-
-// Load Environment Variables
-require("dotenv").config({
-  path: Path.join(process.cwd(), `./env/.${process.env.NODE_ENV}.env`),
-});
 
 /**
  * Sync your Postman with the current routes of this application.
