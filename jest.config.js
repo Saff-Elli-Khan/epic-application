9d3c8a6d4dba92c1762eeb158646eb31b52130a0
Saif Ali Khan <saffellikhan@gmail.com>
2022-07-04 14:35:59 +0500

    Jest configuration updated

--- jest.config.js
+++ jest.config.js
@@ -14,10 +14,10 @@ module.exports = {
   coverageDirectory: "./.coverage",
   coverageThreshold: {
     global: {
-      branches: 0,
-      functions: 0,
-      lines: 0,
-      statements: 0,
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
     },
   },
   setupFilesAfterEnv: ["./src/__tests__/setup.ts"],
