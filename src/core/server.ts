73edfe9b46efc3f3eb1a803d1bde999e57fe31b9
Saif Ali Khan <saffellikhan@gmail.com>
2022-07-05 12:25:07 +0500

    Internal Server Errors Configured

--- src/core/server.ts
+++ src/core/server.ts
@@ -9,11 +9,13 @@ import {
 import { Middlewares } from "./middlewares";
 import { ValidatorException } from "epic-validator";
 import { EpicTokensVerificationException } from "epic-tokens";
+import { Utils } from "@oridune/epic-odm";
+import { Events } from "./globals";
 
 // Prepare Application
 export class Application extends EpicApplication {
   _beforeInit = () => Middlewares(this.Framework);
-  _onRouteError = (err: any, _: Request, res: Response) => {
+  _onRouteError = (err: any, req: Request, res: Response) => {
     if (res.statusCode === 200) {
       // Request Validation Error
       if (err instanceof ValidatorException) res.status(400);
@@ -25,9 +27,27 @@ export class Application extends EpicApplication {
       else res.status(500);
     }
 
+    // Generate Error Code
+    const ErrorID = Utils.uuidShort();
+
+    // Emit Internal Server Error Event
+    if (!(err instanceof ValidatorException))
+      Events.emit("internal_server_error", {
+        errorId: ErrorID,
+        message: err.message || err,
+        stack: err.stack || err,
+      });
+
     // Return Response Object
     return new CreateResponse(
-      err instanceof ValidatorException ? err.List : err.message || err,
+      err instanceof ValidatorException
+        ? err.List
+        : process.env.NODE_ENV !== "production"
+        ? err.message || err
+        : req.translator.tr(
+            `Internal server error! Please contact support with the following Error ID: <>.`,
+            { params: [ErrorID] }
+          ),
       process.env.NODE_ENV !== "production" ? { stack: err.stack || err } : {}
     ).isFalse();
   };
