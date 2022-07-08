889c27b4e8014fc531aefd236f618a74ab42cf0f
Saif Ali Khan <saffellikhan@gmail.com>
2022-07-08 18:45:14 +0500

    Minor updates

--- src/__tests__/APIHome.test.ts
+++ src/__tests__/APIHome.test.ts
@@ -1,5 +1,5 @@
 describe("Test API Home", () => {
-  it("Should return http status 200", async () => {
+  it("GET /api/ APIHome returns http status 200", async () => {
     const { statusCode } = await global.supertest.get("/api/");
     expect(statusCode).toEqual(200);
   });
