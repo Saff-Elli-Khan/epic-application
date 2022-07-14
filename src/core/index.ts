babc919d245f328a11bf9342d109bd9fb06efdca
Saif Ali Khan <saffellikhan@gmail.com>
2022-07-14 14:47:05 +0500

    Minor updates

--- src/core/index.ts
+++ src/core/index.ts
@@ -1,8 +1,12 @@
 import "./loadEnv";
 import { ExecuteJobs } from "./jobs";
 import { HTTPServer } from "./server";
+import { DatabaseAdapter } from "./database";
 
 (async () => {
+  // Sync Database in Development
+  if (process.env.NODE_ENV === "development") await DatabaseAdapter.sync();
+
   // Start Executing Jobs
   await ExecuteJobs();
 
