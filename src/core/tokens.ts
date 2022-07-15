00bfc261892811164d094f84a1d2c9d5ef60cecc
Saif Ali Khan <saffellikhan@gmail.com>
2022-07-15 14:58:42 +0500

    Minor updates

--- src/core/tokens.ts
+++ src/core/tokens.ts
@@ -1,10 +1,14 @@
+import { AppName } from "@App/common";
 import { EpicTokens } from "epic-tokens";
 import { RedisClient } from "./redis";
 
 // Global Tokens Manager
 export const TokensManager = new EpicTokens(
   () => process.env.ENCRYPTION_KEY || "nb4ZHjgVgu0BtM83K97ZNyw8934xUp2Z",
-  { redis: RedisClient }
+  {
+    redis: RedisClient,
+    issuer: AppName,
+  }
 )
   .onEncode((token) => encodeURIComponent(token))
   .onDecode((token) => decodeURIComponent(token));
