d2720b13308daea9e15d57448de17fc0bd7277d7
Saif Ali Khan <saffellikhan@gmail.com>
2022-07-15 14:39:43 +0500

    Minor updates

--- src/common.ts
+++ src/common.ts
@@ -1,11 +1,15 @@
+import Path from "path";
 import { Configuration } from "./exports";
 
 /** Current App Name */
-export const AppName = require("../package.json").name;
+export const AppName = require(Path.join(process.cwd(), "./package.json")).name;
+
+/** Current Repository Name */
+export const RepositoryName = require("../package.json").name;
 
 /** State of the application */
-export const IsPlugin = !!Configuration.plugins[AppName];
+export const IsPlugin = !!Configuration.plugins[RepositoryName];
 
 /** Application's local settings */
 export const Locals: Record<string, any> =
-  Configuration.plugins[AppName]?.locals || Configuration.locals;
+  Configuration.plugins[RepositoryName]?.locals || Configuration.locals;
