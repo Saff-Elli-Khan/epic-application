6fba7a6f021bc21f342ca0f82dc66e2d0480db1d
Saif Ali Khan <saffellikhan@gmail.com>
2022-07-15 12:29:16 +0500

    Minor updates

--- src/core/server.ts
+++ src/core/server.ts
@@ -8,43 +8,44 @@ import {
 } from "@saffellikhan/epic-express";
 import { Middlewares } from "./middlewares";
 import { ValidatorException } from "epic-validator";
-import { EpicTokensVerificationException } from "epic-tokens";
 import { Events } from "./events";
 
 // Prepare Application
 export class Application extends EpicApplication {
   _beforeInit = () => Middlewares(this.Framework);
   _onRouteError = (err: any, req: Request, res: Response) => {
-    if (res.statusCode === 200) {
-      // Request Validation Error
-      if (err instanceof ValidatorException) res.status(400);
-      // Authorization Failed
-      else if (err instanceof EpicTokensVerificationException) res.status(403);
+    // If the status code was not changed than predict it
+    if (res.statusCode === 200)
+      if (err instanceof ValidatorException)
+        // Request Validation Error
+        res.status(400);
       // Page Not Found
       else if (err?.message === "Not Found") res.status(404);
-      // Unknown Error
+      // Internal Server Error
       else res.status(500);
-    }
 
     // Emit Internal Server Error Event
-    if (!(err instanceof ValidatorException))
+    if (res.statusCode >= 500) {
       Events.emit("internal_server_error", {
         errorId: req.id,
         message: err.message || err,
         stack: err.stack || err,
       });
 
+      return new CreateResponse(
+        process.env.NODE_ENV !== "production"
+          ? err.message || err
+          : req.translator.tr(
+              `Internal server error! Please contact support with the following Error ID: <>.`,
+              { params: [req.id] }
+            ),
+        process.env.NODE_ENV !== "production" ? { stack: err.stack || err } : {}
+      ).isFalse();
+    }
+
     // Return Response Object
     return new CreateResponse(
-      err instanceof ValidatorException
-        ? err.List
-        : process.env.NODE_ENV !== "production"
-        ? err.message || err
-        : req.translator.tr(
-            `Internal server error! Please contact support with the following Error ID: <>.`,
-            { params: [req.id] }
-          ),
-      process.env.NODE_ENV !== "production" ? { stack: err.stack || err } : {}
+      err?.List || err?.message || err || "Unknown error occured!"
     ).isFalse();
   };
 }
