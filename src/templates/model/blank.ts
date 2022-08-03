0d8e645999f69142e4f4dbf809427501c158cbf7
Saif Ali Khan <saffellikhan@gmail.com>
2022-08-03 19:48:47 +0500

    Minor updates

--- src/templates/model/blank.ts
+++ src/templates/model/blank.ts
@@ -3,5 +3,8 @@ import { Model, BaseModel, Field } from "@oridune/epic-odm";
 @Model<SampleModel>()
 export class SampleModel extends BaseModel {
   @Field()
+  accountId!: string;
+
+  @Field()
   createdBy!: string;
 }
