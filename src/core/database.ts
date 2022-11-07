5ae268e4b846be5ddf5f29b80dab3f3dd225b782
Saif Ali Khan <saffellikhan@gmail.com>
2022-11-07 21:53:09 +0500

    Framework updated

--- src/core/database.ts
+++ src/core/database.ts
@@ -1,11 +1,15 @@
 import { NODE_ENV } from "@App/common";
 import { MongoDBAdapter } from "@oridune/epic-odm";
 import { LoadModules } from "./helpers/loaders";
+import { RedisClient } from "./redis";
 
 // Create Database Adapter
 export const DatabaseAdapter = new MongoDBAdapter(
   LoadModules("model"),
   process.env.DATABASE_URL ||
     "mongodb://localhost:27017/" + process.env.NODE_ENV,
-  { logs: process.env.NODE_ENV !== NODE_ENV.PRODUCTION }
+  {
+    redis: RedisClient,
+    logs: process.env.NODE_ENV !== NODE_ENV.PRODUCTION,
+  }
 );
