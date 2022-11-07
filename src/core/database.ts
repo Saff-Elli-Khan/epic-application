6c86b44e846aee7973fb066c57ef68ffe6a075ff
Saif Ali Khan <saffellikhan@gmail.com>
2022-11-07 20:30:01 +0500

    Framework updated

--- src/core/database.ts
+++ src/core/database.ts
@@ -7,6 +7,5 @@ export const DatabaseAdapter = new MongoDBAdapter(
   LoadModules("model"),
   process.env.DATABASE_URL ||
     "mongodb://localhost:27017/" + process.env.NODE_ENV,
-  {},
-  process.env.NODE_ENV !== NODE_ENV.PRODUCTION
+  { logs: process.env.NODE_ENV !== NODE_ENV.PRODUCTION }
 );
